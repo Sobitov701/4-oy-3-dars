6e74b5014f52b653871217a1c33e47f49ece028c
Sobitov701 <abdurasul03sobitaliyev>
2024-11-28 20:10:02 +0300

    all add

---
+++ index.js
@@ -0,0 +1,700 @@
+let num1 = +prompt("son");
+if (num1 > 0) {
+  num1 = num1 + 3;
+}
+
+console.log(num1);
+
+let num2 = +prompt("son");
+let $num2 = +prompt("son");
+
+if (num2 > $num2) {
+  console.log(num2);
+} else {
+  console.log($num2);
+}
+
+let num3 = +prompt("yosh");
+if (num3 > 18) {
+  console.log("siz balogat yoshidasiz");
+} else {
+  console.log("siz voyaga yetmagansiz");
+}
+
+let num4 = +prompt("son");
+if (num4 <= 6) {
+  console.log("ish kuni");
+} else {
+  console.log("dam olish kuni");
+}
+
+let num5 = +prompt("son");
+if (num5 > 0) {
+  console.log("iliq");
+} else {
+  console.log("sovuq");
+}
+
+let num6 = +prompt("son");
+if (num6 % 2 == 0) {
+  console.log(num6 * 2);
+} else {
+  console.log(num6 * 3);
+}
+
+let num7 = +prompt("son");
+if (num7 % 5 == 0) {
+  console.log("Fizz");
+} else if (num7 % 3 == 0) {
+  console.log("Buzz");
+} else {
+  console.log(num7);
+}
+
+let num8 = +prompt("son");
+let $num8 = +prompt("son");
+if (num8 == $num8) {
+  console.log(num8 * $num8);
+} else {
+  console.log(num8 + $num8);
+}
+
+let num9 = +prompt("son");
+if (num9 == 5) {
+  console.log("A`lo");
+} else if ((num9 = 4)) {
+  console.log("yaxshi");
+} else if (num9 >= 3) {
+  console.log("qoniqali");
+} else {
+  console.log("qoniqarsiz");
+}
+
+let num10 = +prompt("son");
+
+if (num10 > 100) {
+  console.log("20% chegirma");
+} else {
+  console.log("10% chaegirma");
+}
+
+let num11 = +prompt("son");
+let $num11 = +prompt("son");
+
+if (num11 % $num11 == 0) {
+  console.log("ha");
+} else {
+  console.log("yoq");
+}
+
+let num12 = +prompt("son ");
+if (num12 == 11 && 12 && 1) {
+  console.log("qish");
+} else if (num12 == 2 && 3 && 4) {
+  console.log("bahor");
+} else if (num12 == 5 && 6 && 7) {
+  console.log("yoz");
+} else if (num12 == 8 && 9 && 10) {
+  console.log("kuz");
+}
+
+let _num13 = +prompt("son");
+let $num13 = +prompt("son");
+let num13 = +prompt("son");
+
+if (num13 + _num13 + $num13 > 100) {
+  console.log("katta");
+} else {
+  console.log("kichik");
+}
+
+let num14 = +prompt("son");
+
+if (num14 > 99 && num14 < 1000) {
+  console.log("uch xonali");
+} else {
+  console.log("uch xonali emas");
+}
+
+let num15 = +prompt("son");
+let _num15 = +prompt("son");
+
+if (num15 > _num15) {
+  console.log(num15 - _num15);
+} else if (num15 < _num15) {
+  console.log(_num15 - num15);
+}
+
+let num16 = +prompt("son");
+
+if (num16 < 0) {
+  console.log(num16 * -1);
+} else {
+  console.log(num16 * 2);
+}
+
+let num17 = +prompt("son");
+
+if (nu17 % 4 == 0 && num17 % 100 == 0) {
+  console.log("kabisa yili ");
+} else {
+  console.log("kabisa yili emas");
+}
+
+let num18 = +prompt("son");
+let _num18 = +prompt("son");
+
+if (num18 > _num18) {
+  console.log(1);
+} else if (num18 == _num18) {
+  console.log(0);
+} else if (num18 < _num18) {
+  console.log(-1);
+}
+
+let num19 = +prompt("son");
+let _num19 = +prompt("son");
+
+if (num19 % 2 == 0) {
+  console.log(num19 + _num19);
+} else {
+  console.log(num19 - _num19);
+}
+
+let num20 = +prompt("son");
+
+if (num20 % 5 == 0 && num20 % 3 == 0) {
+  console.log("fizzbuzz");
+} else if (num20 % 5 == 0) {
+  console.log("fizz");
+} else if (num20 % 3 == 0) {
+  console.log("bizz");
+}
+
+let num21 = +prompt("son");
+let _num21 = +prompt("son");
+let $num21 = +prompt("son");
+
+let conuter = 0;
+
+if (num21 <= _num21 && _num21 < $num21) {
+  console.log("osuvchi");
+} else {
+  console.log("osuvchi emas");
+}
+
+let num22 = +prompt("son");
+
+if (num22 > 10 && num22 < 100) {
+  console.log("oraliqda");
+} else {
+  console.log("orilqda emas");
+}
+
+let num23 = +prompt("son");
+let _num23 = +prompt("son");
+
+if (num23 < 10 && _num23 < 10) {
+  console.log("ichida");
+} else {
+  console.log("tashqarida");
+}
+
+let num24 = +prompt("son");
+
+if (num24 % 2 == 0 || num24 % 3 == 0) {
+  console.log("tub son emas");
+} else {
+  console.log("tub son");
+}
+
+let num25 = +prompt("son");
+let $num25 = +prompt("son");
+
+if (num25 * $num25 > 0) {
+  console.log("musbat");
+} else if (num25 * $num25 < 0) {
+  console.log("manfiy ");
+}
+
+let num26 = +prompt("son");
+let $num26 = +prompt("son");
+let _num26 = +prompt("son");
+
+if ((num26 + $num26) ** 2 == _num26) {
+  console.log("uch burchak yasash mumkin ");
+} else {
+  console.log("mumkin emas");
+}
+
+let num27 = +prompt("son");
+
+let num28 = +prompt("son");
+
+if (num28 >= 7 && num28 <= 17) {
+  console.log("ish vaqti");
+} else {
+  console.log("ish vaqti emas");
+}
+
+let num29 = +prompt("son");
+let _num29 = +prompt("son");
+
+if (num29 % _num29 == 0) {
+  console.log("boluvchi");
+} else {
+  console.log("boluvchi emas");
+}
+
+let num30 = +prompt("son");
+
+if (condition) {
+}
+
+let num31 = +prompt("son");
+let _num31 = +prompt("son");
+let $num31 = +prompt("son");
+let n_um31 = +prompt("son");
+
+if ((num31 = _num31 = $num31 = n_um31)) {
+  console.log("teng ");
+} else {
+  console.log("teng emas");
+}
+
+let num32 = +prompt("son");
+
+if (condition) {
+}
+
+let num33 = +prompt("son");
+let _num33 = +prompt("son");
+
+if (num33 > _num33 + 100) {
+  console.log(num33);
+} else if (_num33 > num33 + 100) {
+  console.log(_num33);
+} else if (num33 > _num33) {
+  console.log(_num33);
+}
+
+let nom37 = +prompt("son");
+let _nom37 = +prompt("son");
+let $nom37 = +prompt("son");
+
+// ax2+bx+c=0 a=!0
+
+if (nom37 != 0) {
+  console.log("Kvadrat tenglama");
+} else {
+  console.log("Kvadrat tenglama emas");
+}
+
+let num38 = +prompt("son");
+if (num38 % 7 == 0 && num38 % 3 == 0) {
+  console.log("Bo'linadi");
+} else {
+  console.log("Bo'linmaydi");
+}
+
+let num40 = +prompt("son");
+let _num40 = +prompt("son");
+
+if (num40 ** 2 > num40 ** 2) {
+  console.log("katta");
+} else {
+  console.log("kichik");
+}
+
+// VAZIFA
+
+let num41 = +prompt("son");
+let $num41 = +prompt("son");
+
+if (num41 > 0 && $num41 > 0) {
+  console.log("sonlar musbat");
+} else {
+  console.log("sonlar manfiy");
+}
+
+let num42 = +prompt("son");
+let $num42 = +prompt("son");
+let _num42 = +prompt("son");
+
+let condition = 0;
+
+if (num42 < 0) {
+  console.log(++condition);
+}
+if ($num42 < 0) {
+  console.log(++condition);
+}
+if (_num42 < 0) {
+  console.log(++condition);
+}
+
+let num43 = +prompt("son");
+
+if (num43 % 2 == 0 && num43 > 0) {
+  console.log("son musbat va juft");
+}
+
+let num44 = +prompt("son");
+let $num44 = +prompt("son");
+
+if (num44 + $num44 < num44 * $num44) {
+  console.log("Qoshmasidan katta");
+} else {
+  console.log("qoshmasidan kichkina");
+}
+
+let num45 = +prompt("son");
+
+if (num45 > 100 && num45 < 999) {
+  console.log("berilgan son orliqda ");
+} else {
+  console.log("son orilqda emas");
+}
+
+let num46 = +prompt("son");
+let $num46 = +prompt("son");
+let _num46 = +prompt("son");
+
+if (num46 + $num46 + _num46 > 0) {
+  console.log("sonlar yidisi musbat");
+} else {
+  console.log("sonlar yigindisi manfiy");
+}
+
+let num47 = +prompt("son");
+let $num47 = +prompt("son");
+
+if (num47 == 0 && $num47 == 0) {
+  console.log("sonlar 0 emas");
+}
+
+let num48 = +prompt("sonni kirting");
+let _num48 = +prompt("sonni kirting");
+
+if (num48 * _num48 > 0 && num48 - _num48 < 0) {
+  console.log("kopaytma musbat va ayirma manfiy");
+}
+
+let num49 = +prompt("sonni kirting");
+
+if (10 <= num49 <= 99) {
+  console.log("ikki xonali va musbat");
+}
+
+let num50;
+
+if (num50 % 2 == 0 && num50 % 5 == 0) {
+  console.log("son juft va 5ga karrali");
+}
+
+let num51;
+let _num51;
+let $num51;
+
+if (num51 == $num51 && $num51 == _num51 && num51 == _num51) {
+  console.log("sonnlar teng");
+}
+
+let num52;
+
+if (num52 % 7 == 0 && num52 % 3 != 0) {
+  console.log("son 7ga bolinib 3ga bolinmadi");
+}
+
+let num53;
+let _num53;
+
+if (num53 == 0 || num53 == 0 || (num53 < 0 && num53 < 0)) {
+  console.log("sonnlarni biri 0 yoki musbat");
+}
+
+let num54;
+let _num54;
+let $num54;
+
+let max;
+
+if (num54 > $num54) {
+  max = num54;
+} else {
+  $num54;
+}
+
+if (_num54 > max) {
+  max = _num54;
+}
+if (max > 0) {
+  console.log(true);
+} else {
+  console.log(false);
+}
+
+let num55;
+
+if (num55 < 10 || num > 20) {
+  console.log("10 va 20 orasida emas");
+}
+
+let num56;
+let _num56;
+
+if (num56 + _num56 < 0) {
+  console.log("sonlar yigindisi musbat");
+} else if (num56 * _num56 > 0) {
+  console.log("kopaytmasi musbat");
+}
+
+let num57;
+let _num57;
+let $num57;
+
+if (num57 && $num57 && _num57 % 2 == 0) {
+  console.log("sonlarni eng kichgi musbat");
+}
+
+let num58;
+let _num58;
+let $num58;
+
+if (num58 % 5 == 0) {
+  console.log("oxirgi raqami 5 yoki 0");
+} else if ($num58 % 5 == 0) {
+  console.log("oxirgi raqami 0 yoik 5");
+} else if (_num58 % 5 == 0) {
+  console.log("oxirgi raqami 0 yoki5");
+}
+
+let num59;
+let _num59;
+
+if ((num59 > 0 && _num59 < 0) || (num59 < 0 && _num59 > 0)) {
+  console.log("1 son musbat va 2son manfiy");
+}
+
+let num60;
+let _num60;
+let $num60;
+
+if (num60 % 2 == 0 || $num60 % 2 == 0 || _num60 % 2 == 0) {
+  console.log("hech bolmaganda bittasi musbat");
+}
+
+let num62;
+let _num62;
+
+if (num62 % _num62 == 0 || _num62 % num62 == 0) {
+  console.log("son karrali");
+}
+
+let num63;
+let _num63;
+let $num63;
+
+if (num63 + $num63 + _num63 == 0 || num63 * $num63 * _num63 < 0) {
+  console.log("yigindisi 0 kopaytma manfiy");
+}
+
+let num65;
+let _num65;
+
+if (num65 % 2 == 0 && _num65 % 2 == 1) {
+  console.log(true);
+}
+
+let num66;
+
+if (num66 > 0 && num66 > 99 && num66 < 1000) {
+  console.log(true);
+}
+
+let num67;
+let _num67;
+let $num67;
+
+if (num67 + $num67 + _num67 > 100 || num67 * $num67 * _num67 > 0) {
+  console.log(true);
+}
+
+let num68;
+let _num68;
+
+if (num68 > 10 && _num68 < 0) {
+  console.log(true);
+}
+
+let num69;
+
+if (num69 % 2 == 0 && num69 > 0) {
+  console.log(true);
+}
+
+let num70;
+let _num70;
+
+if ((num70 > 0 && _num70 < 0) || (num70 < 0 && _num70 > 0)) {
+  console.log(true);
+}
+
+let num71;
+
+if (num71 % 4 == 0 && num71 % 8 != 0) {
+  console.log(true);
+}
+
+let num72;
+let _num72;
+let $num72;
+
+if (
+  (num72 > 0 && _num72 > 0 && $num72 < 0) ||
+  (num72 > 0 && _num72 < 0 && $num72 > 0) ||
+  (num72 < 0 && _num72 > 0 && $num72 > 0)
+) {
+  console.log(true);
+}
+
+let num74;
+let _num74;
+
+if (num74 && _num74 < num74 + _num74) {
+  console.log(true);
+}
+
+let num75;
+
+if (num75 > 0 && num75 < 100) {
+  console.log(true);
+}
+
+let num76;
+let _num76;
+
+if (num76 % 2 == 0 || _num76 % 2 == 0 || num76 * _num76 > 20) {
+  console.log(true);
+}
+
+let num77;
+let _num77;
+let $num77;
+
+if (num77 % 10 == 0 || $num77 % 10 == 0 || _num77 % 10 == 0) {
+  console.log(true);
+}
+
+let num78;
+
+if (num78 % 4 == 0) {
+  console.log(true);
+}
+
+let num79;
+let _num79;
+let $num79;
+
+if (num79 + $num79 > _num79 || num79 + _num79 || $num79 + _num79 > num79) {
+  console.log(true);
+}
+
+let num81;
+let _num81;
+let $num81;
+
+if (num81 <= _num81 && _num81 <= $num81) {
+  console.log(true);
+}
+
+let num82;
+let _num82;
+let $num82;
+
+if (
+  (num82 < $num82 && $num82 < _num82) ||
+  (num82 > _num82 && _num82 > $num82)
+) {
+  console.log(true);
+}
+
+let num83;
+let _num83;
+
+if (num83 % 2 != 0 && _num83 % 2 != 0) {
+  console.log(true);
+}
+
+let num84;
+let _num84;
+
+if (num84 % 2 != 0 || _num84 % 2 != 0) {
+  console.log(true);
+}
+
+let num85;
+let _num85;
+
+if (
+  (num85 % 2 == 0 && _num85 % 2 != 0(2 != 0)) ||
+  (num85 % 2 != 0 && _num85 % 2 == 0)
+) {
+  console.log(true);
+}
+
+let num86;
+let _num86;
+
+if (
+  (num86 % 2 == 0 && _num86 % 2 == 0) ||
+  (num86 % 2 != 0 && _num86 % 2 != 0)
+) {
+  console.log(true);
+}
+
+let num87;
+let _num87;
+let $num87;
+
+if (num87 > 0 && $num87 > 0 && _num87 > 0) {
+  console.log(true);
+}
+
+let num88;
+let _num88;
+let $num88;
+
+if (num88 > 0 || $num88 > 0 || _num88 > 0) {
+  console.log(true);
+}
+
+let num89;
+let _num89;
+let $num89;
+
+if (
+  (num89 > 0 && _num89 < 0 && $num89 < 0) ||
+  (num89 < 0 && _num89 < 0 && $num89 > 0) ||
+  (num89 < 0 && _num89 > 0 && $num89 < 0)
+) {
+  console.log(true);
+}
+
+let num90;
+let _num90;
+let $num90;
+
+if (
+  (num90 > 0 && _num90 > 0 && $num90 < 0) ||
+  (num90 > 0 && _num90 < 0 && $num90 > 0) ||
+  (num90 < 0 && _num90 > 0 && $num90 > 0)
+) {
+  console.log(true);
+}
+
+let num91;
+
+if (num91 > 9 && num91 < 100 && num91 % 2 == 0) {
+  console.log(true);
+}
